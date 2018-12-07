1333464d23d9559e968e4d6f0a2e4055142189ee
Roman Dvornov <rdvornov@gmail.com>
2018-12-07 09:32:09 +0300

    Add a test

--- test/complex.js
+++ test/complex.js
@@ -20,4 +20,24 @@ describe('complex tests (mix)', () => {
             ]
         );
     });
+
+    it('get a property pairs with different values (alternative)', () => {
+        const a = { foo: 1, bar: 2, baz: 3 };
+        const b = { a: 4, foo: 1, baz: 5, c: 6 };
+
+        assert.deepEqual(
+            query(`
+                $a; $b;
+                ($a.keys() + $b.keys())
+                .[$a[$] != $b[$]]
+                .({ key: $, a: $a[$], b: $b[$] })
+            `)({ a, b }),
+            [
+                { key: 'bar', a: 2, b: undefined },
+                { key: 'baz', a: 3, b: 5 },
+                { key: 'a', a: undefined, b: 4 },
+                { key: 'c', a: undefined, b: 6 }
+            ]
+        );
+    });
 });
