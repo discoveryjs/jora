8e282671f67e854ec937bb4ce67f592054594169
Roman Dvornov <rdvornov@gmail.com>
2019-12-24 18:27:27 +0300

    Strict to test lib mode

--- test/helpers/lib.js
+++ test/helpers/lib.js
@@ -4,11 +4,16 @@ const libPaths = {
     'dist': 'dist/jora.js',
     'dist-min': 'dist/jora.min.js'
 };
-const mode = libPaths.hasOwnProperty(process.env.MODE) ? process.env.MODE : 'src';
+const mode = process.env.MODE || 'src';
 const libPath = libPaths[mode];
 const postfix = mode === 'src'
     ? (require('../../src/parse').bake ? ' [RAW]' : ' [BAKED]')
     : '';
+
+if (!libPaths.hasOwnProperty(mode)) {
+    console.error(`Mode ${chalk.white.bgRed(mode)} is not supported!\n`);
+    process.exit(1);
+}
 
 console.info('Test lib entry:', chalk.yellow(libPath + postfix));
 
