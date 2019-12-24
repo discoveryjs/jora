4a24186edcf9a7044ebf065d20373cc43b31e96e
Roman Dvornov <rdvornov@gmail.com>
2019-12-24 22:47:58 +0300

    Replace terser with rollup-plugin-terser and related refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,34 @@ const path = require('path');
 const resolve = require('rollup-plugin-node-resolve');
 const json = require('rollup-plugin-json');
 const commonjs = require('rollup-plugin-commonjs');
+const { terser } = require('rollup-plugin-terser');
+
+function replaceContent(map) {
+    return {
+        name: 'file-content-replacement',
+        load(id) {
+            const key = path.relative('', id);
+            if (map.hasOwnProperty(key)) {
+                return map[key](id);
+            }
+        }
+    };
+};
 
 module.exports = {
     input: 'src/index.js',
-    output: {
-        file: 'dist/jora.js',
-        name: 'jora',
-        format: 'umd'
-    },
+    output: [
+        { name: 'jora', format: 'umd', file: 'dist/jora.js' },
+        { name: 'jora', format: 'umd', file: 'dist/jora.min.js' }
+    ],
     plugins: [
         resolve({ browser: true }),
+        replaceContent({
+            'src/parse.js': id => require(id).generateModule(),
+            'package.json': id => `{ "version": "${require(id).version}" }`
+        }),
         commonjs(),
         json(),
-        {
-            name: 'file-content-replacement',
-            load(id) {
-                switch (id) {
-                    case path.resolve('src/parse.js'):
-                        return require('./src/parse').generateModule();
-
-                    case path.resolve('package.json'):
-                        return `{ "version": "${
-                            require('./package.json').version
-                        }" }`;
-                }
-            }
-        }
+        terser({ include: /\.min\./ })
     ]
 };
