6e80781e80b1777da44e4c4df5779aec8cbc7223
Roman Dvornov <rdvornov@gmail.com>
2020-09-15 15:47:27 +0200

    Refactoring of natural sorting

--- src/lang/natural-compare.js
+++ src/lang/natural-compare.js
@@ -1,67 +1,29 @@
 const TYPE_EOF = 0;
 const TYPE_WS = 1;
-const TYPE_NUM = 2;
-const TYPE_DELIM = 3;
+const TYPE_DELIM = 2;
+const TYPE_NUM = 3;
 const TYPE_WORD = 4;
-const T = ['eof', 'ws', 'number', 'delim', 'word'];
+const T = ['eof', 'ws', 'delim', 'number', 'word'];
 let debug = false;
 
-function isWS(code) {
-    return (
-        code === 0x0009 ||  // \t
-        code === 0x000A ||  // \n
-        code === 0x000C ||  // \f
-        code === 0x000D ||  // \r
-        code === 0x0020     // whitespace
-    );
-}
-
-function isDelim(code) {
-    return (
-        code === 0x0021 ||  // !
-        code === 0x0022 ||  // "
-        code === 0x0023 ||  // #
-        code === 0x0024 ||  // $
-        code === 0x0025 ||  // %
-        code === 0x0026 ||  // &
-        code === 0x0027 ||  // '
-        code === 0x0028 ||  // (
-        code === 0x0029 ||  // )
-        code === 0x002a ||  // *
-        // +
-        code === 0x002c ||  // ,
-        // -
-        code === 0x002e ||  // .
-        code === 0x002f ||  // /
-        code === 0x003a ||  // :
-        code === 0x003b ||  // ;
-        code === 0x003c ||  // <
-        code === 0x003d ||  // =
-        code === 0x003e ||  // >
-        code === 0x003f ||  // ?
-        code === 0x0040 ||  // @
-        code === 0x005b ||  // [
-        code === 0x005c ||  // \
-        code === 0x005d ||  // ]
-        code === 0x005e ||  // ^
-        code === 0x005f ||  // _
-        code === 0x0060 ||  // `
-        code === 0x007b ||  // {
-        code === 0x007c ||  // |
-        code === 0x007d ||  // }
-        code === 0x007e ||  // ~
-        code === 0x00a7 ||  // §
-        code === 0x00ab ||  // «
-        code === 0x00b1 ||  // ±
-        code === 0x00b4 ||  // ´
-        code === 0x00bb ||  // »
-        code === 0x2116     // №
-    );
-}
-
-function isDigit(code) {
-    return code >= 0x0030 && code <= 0x0039;
-}
+const safeCharCodeAt = (source, offset) => offset < source.length ? source.charCodeAt(offset) : 0;
+const isSign = (code) => code === 0x002B || code === 0x002D;
+const isDigit = (code) => code >= 0x0030 && code <= 0x0039;
+const isWS = (code) => (
+    code === 0x0009 ||  // \t
+    code === 0x000A ||  // \n
+    code === 0x000C ||  // \f
+    code === 0x000D ||  // \r
+    code === 0x0020     // whitespace
+);
+const isDelim = (code) => (
+    (code > 0x0020 && code < 0x0100) &&  // ascii char
+    (code < 0x0041 || code > 0x005A) &&  // not A..Z
+    (code < 0x0061 || code > 0x007A) &&  // not a..z
+    (code < 0x0030 || code > 0x0039) &&  // not 0..9
+    code !== 0x002B &&                   // not +
+    code !== 0x002D                      // not -
+) || code === 0x2116;  /* № */
 
 //  Check if three code points would start a number
 function isNumberStart(first, second, third) {
@@ -69,7 +31,7 @@ function isNumberStart(first, second, third) {
 
     // U+002B PLUS SIGN (+)
     // U+002D HYPHEN-MINUS (-)
-    if (first === 0x002B || first === 0x002D) {
+    if (isSign(first)) {
         // If the second code point is a digit, return true.
         if (isDigit(second)) {
             return 2;
@@ -98,50 +60,31 @@ function isNumberStart(first, second, third) {
     return 0;
 }
 
-// uppercase letter
-// A code point between U+0041 LATIN CAPITAL LETTER A (A) and U+005A LATIN CAPITAL LETTER Z (Z).
-function isUppercaseLetter(code) {
-    return code >= 0x0041 && code <= 0x005A;
-}
-
-function cmpChar(testStr, offset, referenceCode) {
-    let code = testStr.charCodeAt(offset);
-
-    // code.toLowerCase() for A..Z
-    if (isUppercaseLetter(code)) {
-        code = code | 32;
-    }
-
-    return code === referenceCode;
-}
-
 function findDecimalNumberEnd(source, offset) {
-    for (; offset < source.length; offset++) {
-        if (!isDigit(source.charCodeAt(offset))) {
-            break;
-        }
+    while (isDigit(safeCharCodeAt(source, offset))) {
+        offset++;
     }
 
     return offset;
 }
 
 function consumeNumber(source, offset, preventFloat) {
-    var code = source.charCodeAt(offset);
+    let code = safeCharCodeAt(source, offset);
 
     // If the next input code point is U+002B PLUS SIGN (+) or U+002D HYPHEN-MINUS (-),
     // consume it and append it to repr.
-    if (code === 0x002B || code === 0x002D) {
-        code = source.charCodeAt(offset += 1);
+    if (isSign(code)) {
+        code = safeCharCodeAt(source, offset += 1);
     }
 
     // While the next input code point is a digit, consume it and append it to repr.
     if (isDigit(code)) {
         offset = findDecimalNumberEnd(source, offset + 1);
-        code = source.charCodeAt(offset);
+        code = safeCharCodeAt(source, offset);
     }
 
     // If the next 2 input code points are U+002E FULL STOP (.) followed by a digit, then:
-    if (code === 0x002E && isDigit(source.charCodeAt(offset + 1))) {
+    if (code === 0x002E && isDigit(safeCharCodeAt(source, offset + 1))) {
         if (preventFloat) {
             return offset;
         }
@@ -153,7 +96,7 @@ function consumeNumber(source, offset, preventFloat) {
         expectedEnd = findDecimalNumberEnd(source, expectedEnd);
 
         // If next char is U+002E FULL STOP (.), then don't consume
-        if (expectedEnd < source.length && source.charCodeAt(expectedEnd) === 0x002E) {
+        if (safeCharCodeAt(source, expectedEnd) === 0x002E) {
             return offset;
         }
 
@@ -162,14 +105,15 @@ function consumeNumber(source, offset, preventFloat) {
 
     // If the next 2 or 3 input code points are U+0045 LATIN CAPITAL LETTER E (E)
     // or U+0065 LATIN SMALL LETTER E (e), ... , followed by a digit, then:
-    if (cmpChar(source, offset, 101 /* e */)) {
-        var sign = 0;
-        code = source.charCodeAt(offset + 1);
+    code = safeCharCodeAt(source, offset);
+    if (code === 0x0045 /* e */ || code === 0x0065 /* E */) {
+        let sign = 0;
+        code = safeCharCodeAt(source, offset + 1);
 
         // ... optionally followed by U+002D HYPHEN-MINUS (-) or U+002B PLUS SIGN (+) ...
-        if (code === 0x002D || code === 0x002B) {
+        if (isSign(code)) {
             sign = 1;
-            code = source.charCodeAt(offset + 2);
+            code = safeCharCodeAt(source, offset + 2);
         }
 
         // ... followed by a digit
@@ -182,55 +126,55 @@ function consumeNumber(source, offset, preventFloat) {
     return offset;
 }
 
-function getTwoCharCodes(source, offset) {
-    return [source.charCodeAt(offset) || 0, source.charCodeAt(offset + 1) || 0];
-}
-
 function getPart(source, offset, preventFloat, preventSign) {
     if (offset >= source.length) {
-        return { type: TYPE_EOF };
+        return TYPE_EOF;
     }
 
-    let a = source.charCodeAt(offset);
+    let a = safeCharCodeAt(source, offset);
 
     // Whitespace
     if (isWS(a)) {
         let end = offset + 1;
 
-        while (end < source.length && isWS(source.charCodeAt(end))) {
+        while (isWS(safeCharCodeAt(source, end))) {
             end++;
         }
 
-        return { type: TYPE_WS, len: end - offset };
+        return TYPE_WS | (end - offset << 3);
     }
 
     // Delim sequence
     // console.log(source[offset], isDelim(a), a.toString(16), preventSign)
-    if (isDelim(a) || (preventSign && (a === 0x002b /* + */ || a === 0x002d /* - */))) {
+    if (isDelim(a) || (preventSign && isSign(a))) {
         let end = offset + 1;
+        let b = a;
 
-        while (end < source.length && isDelim(source.charCodeAt(end))) {
-            end++;
-        }
+        do {
+            a = b;
+            b = safeCharCodeAt(source, end++);
+        } while (isDelim(b) || b === a);
 
-        return { type: TYPE_DELIM, len: end - offset };
+        return TYPE_DELIM | (end - offset - 1 << 3);
     }
 
     // Number
-    let [b, c] = getTwoCharCodes(source, offset + 1);
+    let b = safeCharCodeAt(source, offset + 1);
+    let c = safeCharCodeAt(source, offset + 2);
     if (isNumberStart(a, b, c)) {
-        return { type: TYPE_NUM, len: consumeNumber(source, offset, preventFloat) - offset };
+        return TYPE_NUM | (consumeNumber(source, offset, preventFloat) - offset << 3);
     }
 
     // Word
-    let end = offset + 3;
+    let end = offset;
     do {
-        [a, b, c] = [b, c, source.charCodeAt(end++)];
-    } while (end - 3 < source.length && !isWS(a) && !isDelim(a) && !isDigit(a));
+        a = b;
+        b = c;
+        c = safeCharCodeAt(source, 3 + end++);
+    } while (end < source.length && !isWS(a) && !isDelim(a) && !isDigit(a));
 
-    return { type: TYPE_WORD, len: end - offset - 3 };
+    return TYPE_WORD | (end - offset << 3);
 }
-
 
 function naturalCompare(a, b) {
     let offsetA = 0;
@@ -242,25 +186,31 @@ function naturalCompare(a, b) {
     let firstPart = true;
 
     do {
-        const { type: typeA, len: lenA } = getPart(a, offsetA, preventFloat, preventSign);
-        const { type: typeB, len: lenB } = getPart(b, offsetB, preventFloat, preventSign);
+        const partA = getPart(a, offsetA, preventFloat, preventSign);
+        const partB = getPart(b, offsetB, preventFloat, preventSign);
+        const typeA = partA & 7;
+        const lenA = partA >> 3;
+        const typeB = partB & 7;
+        const lenB = partB >> 3;
 
         if (debug) {
             console.log({
-                typeA: T[typeA], lenA, substrA: a.slice(offsetA, offsetA + lenA),
-                typeB: T[typeB], lenB, substrB: b.slice(offsetB, offsetB + lenB)
+                typeA: T[typeA], lenA, substrA: a.substr(offsetA, lenA),
+                typeB: T[typeB], lenB, substrB: b.substr(offsetB, lenB)
             });
         }
 
         if (typeA !== typeB && firstPart) {
             if ((typeA === TYPE_WS || typeA === TYPE_DELIM) && (typeB === TYPE_NUM || typeB === TYPE_WORD)) {
                 postCmpResult = 1;
+                postCmpResultType = typeA;
                 offsetA += lenA;
                 continue;
             }
 
             if ((typeB === TYPE_WS || typeB === TYPE_DELIM) && (typeA === TYPE_NUM || typeA === TYPE_WORD)) {
                 postCmpResult = -1;
+                postCmpResultType = typeB;
                 offsetB += lenB;
                 continue;
             }
@@ -275,105 +225,77 @@ function naturalCompare(a, b) {
         preventFloat = false;
         preventSign = false;
 
-        switch (typeA) { // both parts are the same type
-            case TYPE_EOF:
-                return postCmpResult;
+        // both parts are the same type, no matter which type to test
+        if (typeA === TYPE_EOF) {
+            return postCmpResult;
+        }
 
-            case TYPE_WS:
-            case TYPE_DELIM: {
-                const substrA = a.slice(offsetA, offsetA + lenA);
-                const substrB = b.slice(offsetB, offsetB + lenB);
+        // find difference in substr
+        const minLength = lenA < lenB ? lenA : lenB;
+        let substrDiff = lenA - lenB;
+        let substrDiffIdx = 0;
+        for (; substrDiffIdx < minLength; substrDiffIdx++) {
+            const cA = a[offsetA + substrDiffIdx];
+            const cB = b[offsetB + substrDiffIdx];
 
-                // TODO: replace substrA !== substrB for cmpStr(a, b): -1 | 0 | 1
-                if (substrA !== substrB && (postCmpResult === 0 || typeA > postCmpResultType)) {
+            if (cA !== cB) {
+                substrDiff = cA < cB ? -1 : 1;
+                break;
+            }
+        }
+
+        // both parts are the same type, no matter which type to test
+        if (typeA === TYPE_WS || typeA === TYPE_DELIM) {
+            if (substrDiff !== 0 && (postCmpResult === 0 || typeA > postCmpResultType)) {
+                postCmpResultType = typeA;
+                postCmpResult = substrDiff;
+            }
+
+            preventFloat = a[offsetA + lenA - 1] === '.';
+        } else if (typeA === TYPE_NUM) {
+            preventSign = true;
+
+            if (substrDiff !== 0) {
+                const numDiff = a.substr(offsetA, lenA) - b.substr(offsetB, lenB);
+
+                if (numDiff !== 0) {
+                    return numDiff;
+                }
+
+                if (postCmpResult === 0 || typeA > postCmpResultType) {
+                    const afc = safeCharCodeAt(a, offsetA);
+                    const bfc = safeCharCodeAt(b, offsetB);
+                    const order = afc === 0x002D ? -1 : 1;
+
+                    // a/b  -  o  +
+                    //   -  0 -1 -1
+                    //   o  1  0 -1
+                    //   +  1  1  0
+
                     postCmpResultType = typeA;
-                    postCmpResult = substrA < substrB ? -1 : 1;
+                    postCmpResult = afc !== bfc && (afc === 0x002D /* - */ || bfc === 0x002B /* + */)
+                        ? -1
+                        : afc !== bfc && (afc === 0x002B /* + */ || bfc === 0x002D /* - */)
+                            ? 1
+                            : (lenA !== lenB ? lenA < lenB : substrDiff < 0) ? -order : order;
                 }
-
-                preventFloat = a[offsetA + lenA - 1] === '.';
-                break;
             }
+        } else { // typeA === TYPE_WORD
+            if (substrDiff !== 0) {
+                if (substrDiffIdx < minLength) {
+                    // case insensitive checking
+                    let cnA = a[offsetA + substrDiffIdx].toLowerCase();
+                    let cnB = b[offsetB + substrDiffIdx].toLowerCase();
 
-            case TYPE_NUM: {
-                const substrA = a.slice(offsetA, offsetA + lenA);
-                const substrB = b.slice(offsetB, offsetB + lenB);
-
-                preventSign = true;
-
-                if (substrA !== substrB) { // TODO: replace for cmpStr(a, b): -1 | 0 | 1
-                    const diff = Math.sign(Number(substrA) - Number(substrB));
-
-                    if (diff !== 0) {
-                        return diff;
-                    }
-
-                    if (postCmpResult === 0 || typeA > postCmpResultType) {
-                        const afc = substrA[0];
-                        const bfc = substrB[0];
-
-                        postCmpResultType = typeA;
-
-                        if (afc !== bfc) { // first chars are not equal
-                            if (afc === '-') {
-                                postCmpResult = -1;
-                                break;
-                            }
-
-                            if (afc === '+') {
-                                postCmpResult = 1;
-                                break;
-                            }
-
-                            if (bfc === '-') {
-                                postCmpResult = 1;
-                                break;
-                            }
-
-                            if (bfc === '+') {
-                                postCmpResult = -1;
-                                break;
-                            }
-                        } else if (afc === '-') { // if both start with minus, then change sign of difference
-                            postCmpResult = lenA !== lenB
-                                ? (lenA < lenB ? 1 : -1)
-                                : (substrA < substrB ? 1 : -1);
-                            break;
-                        }
-
-                        postCmpResult = lenA !== lenB
-                            ? (lenA < lenB ? -1 : 1)
-                            : substrA < substrB ? -1 : 1;
+                    if (cnA !== cnB) {
+                        return cnA < cnB ? -1 : 1;
                     }
                 }
 
-                break;
+                return substrDiff;
             }
 
-            case TYPE_WORD: {
-                for (let i = 0, len = Math.min(lenA, lenB); i < len; i++) {
-                    const cA = a[offsetA + i];
-                    const cB = b[offsetB + i];
-
-                    if (cA !== cB) {
-                        let cnA = cA.toLowerCase();
-                        let cnB = cB.toLowerCase();
-
-                        if (cnA !== cnB) {
-                            return cnA < cnB ? -1 : 1;
-                        }
-
-                        return cA < cB ? -1 : 1;
-                    }
-                }
-
-                if (lenA !== lenB) {
-                    return lenA < lenB ? -1 : 1;
-                }
-
-                preventFloat = a[offsetA + lenA - 1] === '.';
-
-                break;
-            }
+            preventFloat = a[offsetA + lenA - 1] === '.';
         }
 
         offsetA += lenA;
