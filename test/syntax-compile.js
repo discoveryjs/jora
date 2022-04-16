f67529f13b2bc69febb88d8da5358cc63e81b242
Roman Dvornov <rdvornov@gmail.com>
2022-04-16 21:06:08 +0200

    One more fix for broken test

--- test/syntax-compile.js
+++ test/syntax-compile.js
@@ -1,19 +1,19 @@
 import assert from 'assert';
 import query from 'jora';
 
-const match = assert.match || ((str, rx) => assert(rx.test(str)));
+const match = assert.match || ((str, rx) => assert(rx.test(str), `${str} doesn't match to ${rx}`));
 
 describe('syntax/compile', () => {
     it('compilation error', () => {
         assert.throws(
             () => {
                 const { ast } = query.syntax.parse('foo()');
-                ast.body.method.reference.name = ','; // break
+                ast.body.method.reference.name = ','; // break AST for a compilation error simulation
                 query.syntax.compile(ast);
             },
             (error) => {
                 match(error.compiledSource, /data,context/);
-                match(error.details.message, /Unexpected token ','/);
+                match(error.details.message, /Unexpected token ('?),\1/);
                 match(error.stack, /Jora query compilation error/);
 
                 return /Jora query compilation error/.test(error);
