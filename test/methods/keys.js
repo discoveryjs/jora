9aca865dc57b30d3209664889c89112b64f372a2
Roman Dvornov <rdvornov@gmail.com>
2023-07-14 00:50:04 +0200

    Refactor unit tests

--- test/methods/keys.js
+++ test/methods/keys.js
@@ -1,22 +1,32 @@
 import assert from 'assert';
 import query from 'jora';
-import data from '../helpers/fixture.js';
 
 describe('keys()', () => {
-    it('basic', () => {
+    it('should return own properties of an object', () => {
         assert.deepEqual(
-            query('.(keys())')(data).sort(),
-            [...new Set(
-                data
-                    .reduce((res, item) => res.concat(Object.keys(item)), [])
-            )].sort()
+            query('{ foo: 1, bar: undefined }.keys()')(),
+            ['foo', 'bar']
+        );
+    });
+
+    it('should return indexes of an array', () => {
+        assert.deepEqual(
+            query('[2, 3, 4].keys()')(),
+            ['0', '1', '2']
+        );
+    });
+
+    it('should return indexes of a string', () => {
+        assert.deepEqual(
+            query('"abcd".keys()')(),
+            ['0', '1', '2', '3']
         );
     });
 
     it('should not fails on non-object values', () => {
-        assert.deepEqual(
-            query('keys()')(null).sort(),
-            []
-        );
+        assert.deepEqual(query('keys()')(), []);
+        assert.deepEqual(query('null.keys()')(), []);
+        assert.deepEqual(query('false.keys()')(), []);
+        assert.deepEqual(query('123.keys()')(), []);
     });
 });
--- test/methods/values.js
+++ test/methods/values.js
@@ -1,35 +1,33 @@
 import assert from 'assert';
 import query from 'jora';
-import data from '../helpers/fixture.js';
 
 describe('values()', () => {
-    it('basic', () => {
+    it('should return values of own properties of an object', () => {
         assert.deepEqual(
-            query('.[filename="1.css"].(values())')(data),
-            [...new Set(
-                Object.values(data[0])
-                    .reduce((res, item) => res.concat(item), [])
-            )]
+            query('{ foo: 1, bar: undefined, baz: 1 }.values()')(),
+            [1] // FIXME: should be [1, undefined, 1]
         );
     });
 
-    it('should return a slice of array', () => {
-        const actual = query('values()')(data);
+    it('should return values of an array', () => {
+        const input = [2, 3, 4];
+        const actual = query('values()')(input);
 
+        assert.notStrictEqual(actual, input);
+        assert.deepEqual(actual, input);
+    });
+
+    it('should return indexes of a string', () => {
         assert.deepEqual(
-            actual,
-            data
-        );
-        assert.notStrictEqual(
-            actual,
-            data
+            query('"abcd".values()')(),
+            ['a', 'b', 'c', 'd']
         );
     });
 
     it('should not fails on non-object values', () => {
-        assert.deepEqual(
-            query('values()')(null),
-            []
-        );
+        assert.deepEqual(query('values()')(), []);
+        assert.deepEqual(query('null.values()')(), []);
+        assert.deepEqual(query('false.values()')(), []);
+        assert.deepEqual(query('123.values()')(), []);
     });
 });
