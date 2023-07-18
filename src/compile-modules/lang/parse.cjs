0a78e02d00e0c3c28f3a409e03d3cb553ffcf359
Roman Dvornov <rdvornov@gmail.com>
2023-07-19 00:56:39 +0200

    Bump jison

--- src/compile-modules/lang/parse.cjs
+++ src/compile-modules/lang/parse.cjs
@@ -5,22 +5,6 @@ const parserPatch = require('./parse-patch.cjs');
 
 const strictParser = new jison.Parser(grammar);
 
-// temp workaround
-if (!Object.hasOwn) {
-    Object.hasOwn = (subject, key) => Object.prototype.hasOwnProperty.call(subject, key);
-}
-if (!String.prototype.replaceAll) {
-    String.prototype.replaceAll = function(pattern, replacement) {
-        return typeof replacement === 'function'
-            ? this.split(pattern).map((s, idx, ar) =>
-                idx !== 0 ? replacement(pattern, ar.slice(0, idx).join('').length, String(this)) + s : s
-            ).join('')
-            : this.split(pattern).join(replacement);
-    };
-
-    console.log('asdasdasd'.replaceAll('s', (...a) => console.log(a) || 'x'));
-}
-
 module.exports = function generateModule() {
     return strictParser
         .generateModule('esm', { packTable: 'advanced' })
