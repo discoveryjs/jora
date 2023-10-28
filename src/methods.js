4b1d4ea4ffd9964f3a25e651f4e8630ad4d7fb70
Roman Dvornov <rdvornov@gmail.com>
2023-10-29 00:56:52 +0200

    Move stableSort into a separate module

--- src/methods.js
+++ src/methods.js
@@ -1,5 +1,6 @@
 import buildin from './lang/compile-buildin.js';
 import { cmp } from './utils/compare.js';
+import { stableSort } from './utils/stable-sort.js';
 import { numbers, count, sum, mean, variance, stdev, min, max, percentile, median } from './utils/statistics.js';
 import { hasOwn, addToSet, addToMapSet, isPlainObject, isRegExp } from './utils/misc.js';
 
@@ -43,34 +44,6 @@ const replaceAll = String.prototype.replaceAll || function(pattern, replacement)
         ? this.replace(pattern, replacement)
         : this.split(pattern).join(String(replacement));
 };
-
-const stableSortSize = isSortStable(20) ? Infinity : isSortStable(10) ? 10 : 0;
-
-function isSortStable(n) {
-    return Array.from({ length: n }, (_, idx) => ({ idx }))
-        .sort((a, b) => (a.idx % 2) - (b.idx % 2))
-        .every((a, idx) =>
-            idx < n / 2 ? (a.idx >> 1 === idx) : Math.ceil(n / 2) + (a.idx >> 1) === idx
-        );
-}
-
-function stableSort(array, cmp) {
-    // check size, e.g. old v8 had stable sort only for arrays with length less or equal 10
-    if (array.length <= stableSortSize) {
-        return array.slice().sort(cmp);
-    }
-
-    return array
-        .map((value, idx) => ({ value, idx }))
-        .sort((a, b) =>
-            (a.value === undefined
-                ? b.value !== undefined
-                : b.value === undefined
-                    ? -1
-                    : cmp(a.value, b.value)) || (a.idx - b.idx)
-        )
-        .map(item => item.value);
-}
 
 export default Object.freeze({
     bool: buildin.bool,
